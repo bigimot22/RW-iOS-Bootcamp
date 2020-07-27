15ebeff7c218a5e43b3dd617581b6769aedbf2f9
Johandre Delgado <johanesweb@gmail.com>
2020-07-27 16:04:50 +0300

    Clean networking code

--- Week09/jQuiz/jQuiz/NetworkingHandler.swift
+++ Week09/jQuiz/jQuiz/NetworkingHandler.swift
@@ -8,27 +8,6 @@
 
 import Foundation
 
-struct QuestionViewModel {
-  let categoryTitle: String
-  let question: String
-  let correctAnswer: String
-  let options: [String]
-  let points: String
-
-  init(clues: [Clue]) {
-    let firstClue = clues.first
-    categoryTitle = firstClue?.category.title ?? "Category"
-    question = firstClue?.question ?? "Question"
-    correctAnswer = firstClue?.answer ?? "Answer"
-    points = String(firstClue?.points ?? 10)
-    var opts = [String]()
-    for clue in clues {
-      opts.append(clue.answer)
-    }
-    options = opts.shuffled()
-  }
-}
-
 class Networking {
   static let sharedInstance = Networking()
   private init(){}
@@ -37,34 +16,31 @@ class Networking {
     let randomClueEndpoint = "http://www.jservice.io/api/random"
     guard let url = URL(string: randomClueEndpoint) else {
       print("Error: Cannot create URL using - \(randomClueEndpoint)")
-      return }
+      return
+    }
     let urlRequest = URLRequest(url: url)
     let session = URLSession.shared
+
     let task = session.dataTask(with: urlRequest) { data, response, error in
       if error != nil {
         print("Error: \(error!.localizedDescription)")
       }
+
       if let data = data {
-        print("Got network data: \(data)")
         do {
           let json = try JSONDecoder().decode([Clue].self, from: data)
-          //          print("\(json)")
-          print("Got category: \(json[0].category.id)")
           let categoryCount = json[0].category.count
           completion(json[0].category.id, categoryCount)
-
         } catch let error {
           print("Decoding error: \(error) \nData in decoding error: \(String(decoding: data, as: UTF8.self))")
         }
       }
     }
     task.resume()
-    print("JD: - Finnish calling network.")
   }
 
 
   func getAllCluesInCategory(categoryId: Int, offset: Int, completion: @escaping ([Clue]?) -> Void) {
-    //http://www.jservice.io/api/clues/?category=11547​.
     let cluesEndpoint: String = "http://www.jservice.io/api/clues/?category=\(categoryId)&offset=\(offset)"
     guard let url = URL(string: cluesEndpoint) else {
       print("Error: Cannot create URL using - \(cluesEndpoint)")
@@ -72,20 +48,18 @@ class Networking {
     }
     let urlRequest = URLRequest(url: url)
     let session = URLSession.shared
+
     let task = session.dataTask(with: urlRequest) { data, response, error in
       if let response = response {
-        print(response.url ?? "url")
+        print(response.url ?? "response.url is nil.")
       }
       if let error = error {
         print(error)
       }
 
       if let data = data {
-        print("Got network data: \(data)")
         do {
           let json = try JSONDecoder().decode([Clue].self, from: data)
-          //          print("\(json)")
-          print("\(json[0].category.id)")
           let options = Array(json.prefix(4))
           completion(options)
 
@@ -106,24 +80,20 @@ class Networking {
 
     let urlRequest = URLRequest(url: url)
     let session = URLSession.shared
+
     let task = session.dataTask(with: urlRequest) { data, response, error in
-      if let response = response {
-        print(response.url ?? "url")
+      if let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) {
+        print("Image network call status code: \(response.statusCode)")
       }
       if let error = error {
         print(error)
       }
 
       if let data = data {
-        print("Got network data: \(data)")
         completion(data)
       }
     }
     task.resume()
-
-
-
   }
-
 
 }
---
+++ Week09/jQuiz/jQuiz/QuestionViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewModel.swift
+//  jQuiz
+//
+//  Created by Johandre Delgado  on 27.07.2020.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import Foundation
+
+struct QuestionViewModel {
+  let categoryTitle: String
+  let question: String
+  let correctAnswer: String
+  let options: [String]
+  let points: String
+
+  init(clues: [Clue]) {
+    let firstClue = clues.first
+    categoryTitle = firstClue?.category.title ?? "Category"
+    question = firstClue?.question ?? "Question"
+    correctAnswer = firstClue?.answer ?? "Answer"
+    points = String(firstClue?.points ?? 10)
+    var opts = [String]()
+    for clue in clues {
+      opts.append(clue.answer)
+    }
+    options = opts.shuffled()
+  }
+}
