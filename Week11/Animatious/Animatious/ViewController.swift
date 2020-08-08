35d8b2f4e931efdbed9584574f78fc9ef5a1a509
Johandre Delgado <johanesweb@gmail.com>
2020-08-08 12:37:01 +0300

    Add Choice buttons animation

--- Week11/Animatious/Animatious/ViewController.swift
+++ Week11/Animatious/Animatious/ViewController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet private weak var leftButtonLeadingConstraint: NSLayoutConstraint!
+  @IBOutlet private weak var topButtonBottomConstraint: NSLayoutConstraint!
+  @IBOutlet private weak var rightButtonTrailingConstraint: NSLayoutConstraint!
+
+  @IBOutlet private weak var leftButton: UIButton!
+  @IBOutlet private weak var topButton: UIButton!
+  @IBOutlet private weak var rightButton: UIButton!
+
+  private var showOptionButtons = false
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    showOptionButtons = false
+    resetButtonsConstraints()
+    setButtonsAlpha()
+
+  }
+
+  @IBAction private func didTapPlay(_ sender: UIButton) {
+    print("play button tapped. isOpen is: \(showOptionButtons)")
+    showOptionButtons.toggle()
+    playButtonsAnimations()
+  }
+
+  private func playButtonsAnimations() {
+    resetButtonsConstraints()
+
+    UIView.animate(withDuration: 0.3) {
+      self.view.layoutIfNeeded()
+      self.setButtonsAlpha()
+    }
+  }
+
+  private func resetButtonsConstraints() {
+    self.leftButtonLeadingConstraint.constant = self.showOptionButtons ? -100 : 0
+    self.topButtonBottomConstraint.constant = self.showOptionButtons ? -100 : 0
+    self.rightButtonTrailingConstraint.constant = self.showOptionButtons ? 100 : 0
+  }
+
+  private func setButtonsAlpha() {
+    self.leftButton.alpha = self.showOptionButtons ? 1 : 0
+    self.topButton.alpha = self.showOptionButtons ? 1 : 0
+    self.rightButton.alpha = self.showOptionButtons ? 1 : 0
+  }
+
+  private func playSelectedAnimations() {
+
   }
 
 
