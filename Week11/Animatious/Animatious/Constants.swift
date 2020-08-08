5979c3a0b34657a938802392957fb63799807eb3
Johandre Delgado <johanesweb@gmail.com>
2020-08-08 14:47:49 +0300

    Manage  animationObject transform

---
+++ Week11/Animatious/Animatious/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Animatious
+//
+//  Created by Johandre Delgado  on 08.08.2020.
+//  Copyright © 2020 Johandre Delgado . All rights reserved.
+//
+
+import UIKit
+
+enum Colors {
+  static var purple = UIColor(named: "jdPurple")!
+  static var active = UIColor(named: "jdActive")!
+}
--- Week11/Animatious/Animatious/ViewController.swift
+++ Week11/Animatious/Animatious/ViewController.swift
@@ -14,19 +14,32 @@ class ViewController: UIViewController {
   @IBOutlet private weak var topButtonBottomConstraint: NSLayoutConstraint!
   @IBOutlet private weak var rightButtonTrailingConstraint: NSLayoutConstraint!
 
+  @IBOutlet private weak var playButton: UIButton!
   @IBOutlet private weak var leftButton: UIButton!
   @IBOutlet private weak var topButton: UIButton!
   @IBOutlet private weak var rightButton: UIButton!
 
   private var showOptionButtons = false
+  private var readyToAnimate = false
+
+  private var rotation = CGAffineTransform(rotationAngle: .pi / 1)
+  private var scale = CGAffineTransform(scaleX: 1.3, y: 1.3)
+  private var translation = CGAffineTransform(translationX: 200, y: 0)
+  private var color = Colors.active
 
   private lazy var animationObject: UIImageView = {
       let image = UIImageView()
-      image.image =  #imageLiteral(resourceName: "ray_thermometer_window")//imageLiteral(resourceName: "appLogo")
+      image.image =  #imageLiteral(resourceName: "ray_thermometer_window")
       image.contentMode = .scaleAspectFit
       image.translatesAutoresizingMaskIntoConstraints = false
       return image
   }()
+
+  private var playFeedbackColor: UIColor = .purple {
+    didSet {
+      playButton.tintColor = playFeedbackColor
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -40,10 +53,32 @@ class ViewController: UIViewController {
 
   }
 
+  @IBAction private func didTapColorButton(_ sender: UIButton) {
+    print("didTapColorButton: \(1)")
+
+  }
+
+  @IBAction private func didTapExpanseButton(_ sender: UIButton) {
+    print("didTapExpanseButton: \(2)")
+    readyToAnimate = true
+  }
+
+  @IBAction private func didTapRotateButton(_ sender: UIButton) {
+    print("didTapRotateButton: \(3)")
+  }
+
   @IBAction private func didTapPlay(_ sender: UIButton) {
     print("play button tapped. isOpen is: \(showOptionButtons)")
-    showOptionButtons.toggle()
-    playButtonsAnimations()
+    if !showOptionButtons && readyToAnimate {
+      playSelectedAnimations()
+    } else {
+      showOptionButtons.toggle()
+      playButtonsAnimations()
+//      if readyToAnimate {
+//        sender.tintColor = .yellow
+//      }
+    }
+
   }
 
   private func playButtonsAnimations() {
@@ -52,6 +87,7 @@ class ViewController: UIViewController {
     UIView.animate(withDuration: 0.3) {
       self.view.layoutIfNeeded()
       self.setButtonsAlpha()
+      self.playButton.tintColor = self.readyToAnimate ? Colors.active : Colors.purple
     }
   }
 
@@ -68,7 +104,22 @@ class ViewController: UIViewController {
   }
 
   private func playSelectedAnimations() {
+    UIView.animate(withDuration: 1, animations: {
+      self.animationObject.transform = self.scale.concatenating(self.rotation)
+      self.animationObject.backgroundColor = self.color
+    }) { _ in
+      self.resetAnimationObject()
+    }
 
+  }
+
+  private func resetAnimationObject() {
+    UIView.animate(withDuration: 0.6) {
+      self.animationObject.transform = CGAffineTransform.identity
+      self.animationObject.backgroundColor = .clear
+      self.readyToAnimate = false
+      self.playButton.tintColor = Colors.purple
+    }
   }
 
   func addConstraintsToView() {
